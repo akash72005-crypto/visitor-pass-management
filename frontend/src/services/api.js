2897fca0c717b80ab321f8646b0c3d2727b864a0
Akash A <akash72005@gmail.com>
2026-08-09 00:12:11 +0530

    Connect frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://visitor-pass-management-quhw.onrender.com/api",
 });
 
 export default api;
